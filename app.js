300227d68c6766911ac27a8af3e0f0d08b4159fb
hk698 <khanhasnaat111@gmail.com>
2020-11-05 17:25:30 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        sitename: 'After School Classes',
+        lessons: lessons
+    }
+
+})
---
+++ lesson.js
@@ -0,0 +1,72 @@
+let lessons = [
+    {
+        id: 1,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 2,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 3,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 4,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 5,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 6,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 7,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 8,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 9,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    },
+    {
+        id: 10,
+        subject: 'Maths',
+        location: 'London',
+        price: 30,
+        availableSpaces: 5
+    }
+]
